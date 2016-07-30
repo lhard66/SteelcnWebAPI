f4238f64559ca55930781ad3950cb172c16853a5
LiuTianqing <lhard@qq.com>
2016-07-30 17:49:48 +0800

    Completed get a article in cnblogs function

--- SteelcnWebAPI/Common/WebUtinity.cs
+++ SteelcnWebAPI/Common/WebUtinity.cs
@@ -13,8 +13,7 @@ namespace SteelcnWebAPI.Common
         public static string SendRequest(string url,Encoding encoding)
         {
             HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url);
-            //System.Net.Http.HttpClient c = new System.Net.Http.HttpClient(); 建议使用此对象，因为支持异步操作
-            
+            //System.Net.Http.HttpClient c = new System.Net.Http.HttpClient(); 建议使用此对象，因为支持异步操作            
             webRequest.Method = "GET";
             HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse();
             StreamReader sr = new StreamReader(webResponse.GetResponseStream(), encoding);
---
+++ SteelcnWebAPI/Controllers/CnBlogsController.cs
@@ -0,0 +1,32 @@
+﻿using SteelcnWebAPI.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SteelcnWebAPI.Controllers
+{
+    public class CnBlogsController : Controller
+    {
+        // GET: CnBlogs
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string GetArticle(string strUrl)
+        {
+            string strContent = WebUtinity.SendRequest(strUrl, Encoding.UTF8);
+            string strPatter1 = @"<!--done-->\s*<div id=""topics"">[\s\S]*<!--end: topics 文章、评论容器-->";
+            Match match = Regex.Match(strContent, strPatter1);
+            if (!match.Success)
+            {
+                string strPatter2 = @"<div id=""post_detail"">[\s\S]*<a name=""!comments""></a>";
+                match = Regex.Match(strContent, strPatter2);
+            }
+            return match.Value;
+        }
+    }
+}
